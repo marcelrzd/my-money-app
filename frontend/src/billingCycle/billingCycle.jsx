b8542429c70660bf1ee1342bb19626adfc10e986
Marcel Rezende <marcel.rzd@gmail.com>
2022-02-01 13:46:27 -0300

    first commit

---
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react"
+import { bindActionCreators } from "redux"
+import { connect } from 'react-redux'
+
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from "../common/tab/tabs"
+import TabsHeader from "../common/tab/tabsHeader"
+import TabsContent from "../common/tab/tabsContent"
+import TabHeader from "../common/tab/tabHeader"
+import TabContent from "../common/tab/tabContent"
+import { selectTab, showTabs } from "../common/tab/tabActions"
+import BillingCycleList from "./billingCycleList"
+import BillingCycleForm from "./billingCycleForm"
+import { create, update, remove } from "./billingCyclesActions"
+
+class BillingCycle extends Component {
+
+    componentWillMount() {
+        this.props.selectTab('tabList')
+        this.props.showTabs('tabList', 'tabCreate')
+    }
+
+    render(){
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamento' small='Cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                            <TabContent id='tabList'>
+                                <BillingCycleList />
+                            </TabContent>
+                            <TabContent id='tabCreate'>
+                                <BillingCycleForm onSubmit={this.props.create} 
+                                submitLabel='Incluir' submitClass='primary' />
+                            </TabContent>
+                            <TabContent id='tabUpdate'>
+                                <BillingCycleForm onSubmit={ this.props.update } 
+                                submitLabel='Alterar' submitClass='info' />
+                            </TabContent>
+                            <TabContent id='tabDelete'>
+                            <BillingCycleForm onSubmit={ this.props.remove } readOnly={true} 
+                                submitLabel='Excluir' submitClass='danger'  />
+                            </TabContent>
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators(
+    {
+        selectTab, showTabs, create, update, remove
+    }, dispatch
+)
+export default connect(null, mapDispatchToProps)(BillingCycle)
